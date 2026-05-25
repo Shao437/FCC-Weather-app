e43f33d4582c611123135e86312ab3bdc2f3d4c9
Shao437 <grshao437@gmail.com>
2026-05-25 13:35:10 +0800

    Making code more readable and functional

--- weather.py
+++ weather.py
@@ -5,11 +5,24 @@ has_car = True
 has_ride_share_app = True
 
 
-def can_reach_destination(distance_miles, is_raining, has_bike, has_car, has_ride_share_app):
+def can_reach_destination(
+        distance_miles: int, 
+        is_raining: bool, 
+        has_bike: bool, 
+        has_car: bool, 
+        has_ride_share_app: bool
+        ) -> str:
+    
+    '''
+    Determine whether the user can reach their destination based on the distance, 
+    weather, available transportation options and weather travel app
+    '''
+
     can_travel = 'You can reach your destination'
     cannot_travel = 'You cannot reach your destination'
 
-    if not distance_miles:
+
+    if distance_miles <= 0:
         return cannot_travel
         
 
@@ -17,18 +30,12 @@ def can_reach_destination(distance_miles, is_raining, has_bike, has_car, has_rid
         return can_travel
 
 
-    elif distance_miles > 1 and distance_miles <= 6:
-        if has_bike and not is_raining:
-            return can_travel
-        else:
-            return cannot_travel
+    elif distance_miles > 1 and distance_miles <= 6 and has_bike and not is_raining:
+        return can_travel
 
-    elif distance_miles > 6:
-        if has_car or has_ride_share_app:
-            return can_travel
-        else:
-            return cannot_travel
-    else:
-        return cannot_travel
+
+    elif distance_miles > 6 and (has_car or has_ride_share_app):
+        return can_travel
+    return cannot_travel
 
 print(can_reach_destination(distance_miles, is_raining, has_bike, has_car, has_ride_share_app))
